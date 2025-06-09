4c70a5af0f8873f6400a16a9cb0b826940a28a51
WetNoodleDev <dev@wetnoodlesoft.com>
2025-06-09 15:20:12 -0700

    v1.2
    Added start scene

--- Scripts/start_screen.gd
+++ Scripts/start_screen.gd
@@ -1,1 +1,17 @@
 extends Control
+
+@onready var start_button: Button = $CenterContainer/PanelContainer/Button
+var main_scene: PackedScene = preload("res://Scenes/main.tscn")
+
+func _ready() -> void:
+	start_button.pressed.connect(_start_pressed)
+	return
+
+
+func _process(_delta: float) -> void:
+	return
+
+
+func _start_pressed() -> void:
+	get_tree().change_scene_to_packed(main_scene)
+	return
